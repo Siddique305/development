b481c310dbe97a6ec2dc067aac0cd9ce91a42b39
Siddique305 <140276626+Siddique305@users.noreply.github.com>
2023-08-26 19:43:12 +0530

    Update TimeCompare.java
    Made few changes to check time and store time difference in list if the given time strings are different

--- TimeCompare.java
+++ TimeCompare.java
@@ -1,41 +1,62 @@
-	package com.example.demo;
-
-
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class TimeCompare {
     public static void main(String[] args) {
-        String time1 = "1h 5min";
-        String time2 = "1:02:37";
-        int diff = extractMinutes(time2)- extractMinutes(time1);
+        String timeStr1 = "1h 3min 2sec";
+        String timeStr2 = "1:02:37";
+        
+        List<Integer> timeDifference = compareTime(timeStr1, timeStr2);
+        
 
-        boolean minutesAreSame = compareMinutes(time1, time2);
-
-        if (minutesAreSame) {
-            System.out.println("Minutes are the same.");
+        if (timeDifference==null) {
+            System.out.println("Time is same.");
         } else {
-            System.out.println("Minutes are different.");
-            System.out.println("Different : " + Math.abs(diff) + " Minutes");
+            System.out.println("Time is different. Time Difference: "+ timeDifference);
         }
     }
 
-    public static boolean compareMinutes(String time1, String time2) {
-        int minutes1 = extractMinutes(time1);
-        int minutes2 = extractMinutes(time2);
+    public static List<Integer> compareTime(String timeStr1, String timeStr2) {
+        int[] time1 = extractTime(timeStr1);
+        int[] time2 = extractTime(timeStr2);
+        
+        if(time1[0]==time2[0] && time1[1]==time2[1] && time1[2]==time2[2]) {
+        	return null;
+        }
+        else {
+        	return getTimeDifference(time1, time2);
+        }
 
-        return minutes1 == minutes2;
+        
     }
-
-    public static int extractMinutes(String timeStr) {
+    
+    public static int[] extractTime(String timeStr) {
         if (timeStr.contains("h")) {
-            String[] parts = timeStr.split(" ");
-            int minutes = Integer.parseInt(parts[1].replace("min", ""));
-            return  minutes;
+            String[] time = timeStr.split(" ");
+            int hours = Integer.parseInt(time[0].replace("h",""));
+            int minutes = Integer.parseInt(time[1].replace("min", ""));
+            int seconds = Integer.parseInt(time[2].replace("sec", ""));
+            return  new int[]{hours, minutes, seconds};
         } else {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm:ss");
             LocalTime localTime = LocalTime.parse(timeStr, formatter);
-            return  localTime.getMinute();
+            int hours= localTime.getHour();
+            int minutes =  localTime.getMinute();
+            int seconds = localTime.getSecond();
+            
+           return new int[]{hours,minutes,seconds};
         }
+    }
+    
+    public static List<Integer> getTimeDifference(int[] time1, int[] time2){
+    	int totalSecDifference = Math.abs((time2[0]*3600 + time2[1]*60 + time2[2]) -
+    									(time1[0]*3600 + time1[1]*60 + time1[2]));
+    	int hours = totalSecDifference/3600;
+    	int minutes = (totalSecDifference % 3600) / 60;
+        int seconds = totalSecDifference % 60;
+
+        return List.of(hours, minutes, seconds);
+    	
     }
 }
